72bf8d3b88168d1aec7045919aa2012d089ca53c
Scott Guthart <seguthart@gmail.com>
2022-05-26 17:39:36 -0400

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,30 +6,37 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from config import Config
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 db = SQLAlchemy()
 login = LoginManager()
-login.login_view = 'auth.login'
+login.login_view = "auth.login"
 mail = Mail()
 bootstrap = Bootstrap()
 migrate = Migrate()
+cors = CORS()
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
     db.init_app(app)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
+    cors.init_app(app)
 
     from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
 
     from app.main import bp as main_bp
+
     app.register_blueprint(main_bp)
 
     return app
+
 
 from app import models
